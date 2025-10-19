360a941a5944d72c452808272b9108faaced4c02
HeyItsBob36 <166279266+HiMyNameIsBob36@users.noreply.github.com>
2025-10-19 13:29:46 +1100

    Update manifest_subpacks.js

--- manifest_subpacks.js
+++ manifest_subpacks.js
@@ -1,1 +1,65 @@
+{
+    "format_version": 2,
+    "header": {
+        "name": "§l§bUltimate §aSurvival §r§lTexture §6Pack §r§f(§c§lv3.9.8§r§f)§r",
+        "description": "The §lDefault Version§r of the Pack with Health & Damage Indicator for enhancing the experience, reducing obstructions and more\n- §fMade by §dSUN§aBUN§e123",
+        "uuid": "cd905413-d9d2-45be-80a3-1027b3bd3a15",
+        "version": [ 3, 9, 8 ],
+        "min_engine_version": [ 1, 20, 0 ]
+    },
+    "modules": [
+        {
+            "type": "resources",
+            "uuid": "3beebc17-79ca-4854-9dd9-9d607b3f8e11",
+            "version": [ 3, 9, 8 ]
+        }
+    ],
+    "subpacks": [
+        {
+            "folder_name": "noinfo",
+            "name": "§lNO§r Item Info\n\n\n(It doesnt contain Item Info)\n\n\n",
+            "memory_tier": 1
+        },
+        {
+            "folder_name": "basic",
+            "name": "Only §l§bBasic§r Item Info\n\nMob HP & Hostility on Spawn Eggs, Duration of Music Discs, Banner & Firework Pattern Icons.§r\n\n(Every Subpack contains these)",
+            "memory_tier": 2
+        },
+        {
+            "folder_name": "fatlight",
+            "name": "§l§aFAT§r, §l§eLight§r Info\n\nHunger, Saturation, Time To Use Consumables, Armor Points, Max Item Durability, Attack Damage, Light Level Icons.§r\n\n(Including §l§bBasic§r Item Info)",
+            "memory_tier": 3
+        },
+        {
+            "folder_name": "enchantlight",
+            "name": "§l§dEnchant§r, §l§eLight§r Info\n\nEnchantment, Light Level Icons.§r\n\n\n(Including §l§bBasic§r Item Info)",
+            "memory_tier": 4
+        },
+        {
+            "folder_name": "fatenchant",
+            "name": "§l§aFAT§r, §l§dEnchant§r Info\n\nHunger, Saturation, Time To Use Consumables, Armor Points, Max Item Durability, Attack Damage, Enchantment Icons.§r\n\n(Including §l§bBasic§r Item Info)",
+            "memory_tier": 5
+        },
+        {
+            "folder_name": "light",
+            "name": "§l§eLight§r Info\n\nLight Level Icons.§r\n\n\n(Including §l§bBasic§r Item Info)",
+            "memory_tier": 6
+        },
+        {
+            "folder_name": "enchant",
+            "name": "§l§dEnchant§r Summary\n\nEnchantment Icons.§r\n\n\n(Including §l§bBasic§r Item Info)",
+            "memory_tier": 7
+        },
+        {
+            "folder_name": "fat",
+            "name": "§l§aF§rood, §l§aA§rrmor, §l§aT§rools Info\n\nHunger, Saturation, Time To Use Consumables, Armor Points, Max Item Durability, Attack Damage Icons.§r\n\n(Including §l§bBasic§r Item Info)",
+            "memory_tier": 8
+        },
+        {
+            "folder_name": "whole",
+            "name": "Item Info (§lFull§r)\n\nHunger, Saturation, Time To Use Consumables, Armor Points, Max Item Durability, Attack Damage, Enchantment, Light Level Icons.§r\n(Including §l§bBasic§r Item Info)",
+            "memory_tier": 9
+        }
+    ]
+}
 
