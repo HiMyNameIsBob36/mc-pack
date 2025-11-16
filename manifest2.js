b9a47a0c78d8aa3073e192f429d9e052d4d9df41
HeyItsBob36 <166279266+HiMyNameIsBob36@users.noreply.github.com>
2025-11-16 11:41:45 +1100

    Create manifest2.js

---
+++ manifest2.js
@@ -0,0 +1,69 @@
+{
+	"format_version": 2,
+	"header": {
+		"name": "HeyItsBob36",
+		"description": "Download the pack that @HeyItsBob uses in his videos!\n§oNot all assests are original.",
+		"uuid": "24fab5c3-1a6d-497a-b746-53e8bceb6b40",
+		"version": [ 1, 0, 0 ],
+		"min_engine_version": [ 1, 21, 100 ]
+	},
+	"modules": [
+		{
+			"type": "resources",
+			"uuid": "f8a80690-b648-4766-ae79-7a87c11fe805",
+			"version": [ 1, 0, 0 ]
+		}
+	],
+    "subpacks": [
+        {
+            "folder_name": "white",
+            "name": "The following textures will change to §lWHITE§r:\n- Hotbar outline\n- XP bar\n- Hearts\n- Elytra\n- Enchantment glint\n\n",
+            "memory_tier": 1
+        },
+        {
+            "folder_name": "black",
+            "name": "The following textures will change to §l§8BLACK§r:\n- Hotbar outline\n- XP bar\n- Hearts\n- Elytra\n- Enchantment glint\n\n",
+            "memory_tier": 2
+        },
+        {
+            "folder_name": "red",
+            "name": "The following textures will change to §l§cRED§r:\n- Hotbar outline\n- XP bar\n- Hearts\n- Elytra\n- Enchantment glint\n\n",
+            "memory_tier": 3
+        },
+        {
+            "folder_name": "orange",
+            "name": "The following textures will change to §l§6ORANGE§r:\n- Hotbar outline\n- XP bar\n- Hearts\n- Elytra\n- Enchantment glint\n\n",
+            "memory_tier": 4
+        },
+        {
+            "folder_name": "yellow",
+            "name": "The following textures will change to §l§eYELLOW§r:\n- Hotbar outline\n- XP bar\n- Hearts\n- Elytra\n- Enchantment glint\n\n",
+            "memory_tier": 5
+        },
+        {
+            "folder_name": "green",
+            "name": "The following textures will change to §l§aGREEN§r:\n- Hotbar outline\n- XP bar\n- Hearts\n- Elytra\n- Enchantment glint\n\n",
+            "memory_tier": 6
+        },
+        {
+            "folder_name": "lightblue",
+            "name": "The following textures will change to §l§bLIGHT BLUE§r:\n- Hotbar outline\n- XP bar\n- Hearts\n- Elytra\n- Enchantment glint\n\n",
+            "memory_tier": 7
+        },
+        {
+            "folder_name": "blue",
+            "name": "The following textures will change to §l§9BLUE§r:\n- Hotbar outline\n- XP bar\n- Hearts\n- Elytra\n- Enchantment glint\n\n",
+            "memory_tier": 8
+        },
+        {
+            "folder_name": "purple",
+            "name": "The following textures will change to §l§uPURPLE§r:\n- Hotbar outline\n- XP bar\n- Hearts\n- Elytra\n- Enchantment glint\n\n",
+            "memory_tier": 9
+        },
+		{
+            "folder_name": "pink",
+            "name": "The following textures will change to §l§dPINK§r:\n- Hotbar outline\n- XP bar\n- Hearts\n- Elytra\n- Enchantment glint\n\n",
+            "memory_tier": 10
+		}
+    ]
+}
